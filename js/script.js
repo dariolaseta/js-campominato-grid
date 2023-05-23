df98c391bf7ac2955bbf92c1400f4703c4fc3566
dariolaseta <dario.laseta@gmail.com>
2023-05-23 15:12:53 +0200

    created script for create grid and cells

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 const gridElement = document.getElementById("grid");
 const button = document.getElementById("button")
-
+let isCreated = false;
 
 button.addEventListener("click", function(){
     gridElement.classList.add("container");
 
+    if(!isCreated){
+        for (let i = 0; i < 100; i++) {
+            const newCell = createCell(i);
+            
+            gridElement.append(newCell);
+        }
+        isCreated = true;
+    }
+
 });
+
+function createCell(cellNumber) {
+    const cell = document.createElement("div");
+    cell.classList.add("cells");
+    cell.innerHTML = `<span>${cellNumber + 1}</span>`;
+    return cell;
+}
